967b021ff3faff02bd8072e01922e577bfedfbba
HosinoEJ <hosinoeiji@gmail.com>
2026-02-21 00:57:33 +0800

    mod google from post

--- app/server.js
+++ app/server.js
@@ -24,7 +24,7 @@ app.post('/submit', async (req, res) => {
     } = req.body;
 
   // Google Form 的 POST 地址（注意：最後是 formResponse）
-  const googleFormUrl = 'https://docs.google.com/forms/d/e/1FAIpQLScggjQgYutXQrjQDrutyxL0eLaFMktTMRKsFWPffQGavUFspA/viewform?usp=header';
+  const googleFormUrl = 'https://docs.google.com/forms/d/e/1FAIpQLScggjQgYutXQrjQDrutyxL0eLaFMktTMRKsFWPffQGavUFspA/formResponse';
 
   // 對應你的 entry ID
   const formData = new URLSearchParams();
